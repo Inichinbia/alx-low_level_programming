f95ec64502e0ab4e3ce848896ab273cc7b2d174b
Inichinbia <inichinbia.f@gmail.com>
2023-10-09 19:21:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
+#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
- * returns 0
- * */
+ * return : always 0 (sucess)
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
