3495a0ee63e616a3ccfb00e5e374ffd04cf983c0
Inichinbia <inichinbia.f@gmail.com>
2023-10-09 20:00:11 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * returns: always 0 (success)
+ * returns 0
  */
 int main(void)
 {
+	/* This program demonstrates the usage of the main function
+	 * and the puts
+	 * The return value 0 indicates successful execution
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 /**
- * main - entry point 
- * return : always 0 (success)
+ * main - entry point
+ * return:always0(success)
  */
 int main(void)
 {
+	/* This program demonstrates the usage of the main function.
+	 * The return value 0 indicates successful execution
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
