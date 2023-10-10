f49c408fd7b5c7fa0ea04b39de01f4777c363f04
Inichinbia <inichinbia.f@gmail.com>
2023-10-09 20:42:11 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * returns 0
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* This program demonstrates the usage of the main function
-	 * and the puts
-	 * The return value 0 indicates successful execution
+	 /*
+	  * This is void because no integer involved
+	  * main - This is Entry point
+	  * puts is used to print output
+	 * The return 0 indicates successful execution
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 /**
- * main - entry point
- * return:always0(success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* This program demonstrates the usage of the main function.
+	/*
+	 * This program demonstrates the usage of the main function.
 	 * The return value 0 indicates successful execution
 	 */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
